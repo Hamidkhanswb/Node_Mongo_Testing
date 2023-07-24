4cf9c32256c63cb61ecce5320f5909f29cfacd54
hamid.khan@funavry.com <hamid.khan@funavry.com>
2023-07-24 17:45:50 +0500

    ssd

--- app.js
+++ app.js
@@ -9,4 +9,15 @@ app.get('/', function(req, res){
    })
 });
 
+app.get('/home', function(req, res){
+   res.send({
+      message: "hello home screen",
+      data: process.env.name
+   })
+});
+
+app.get('/crash', function(req, res){
+   throw new Error("crashed")
+});
+
 app.listen(3000);
